5db41022e5727b0b6a3c3342b67a454502a299a7
muttsu-623 <mutsumi1996@gmail.com>
2018-01-26 18:39:55 +0900

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {user: {name: "Example User",
+                                       email: "user@example.com",
+                                       password: "password",
+                                       password_confirmation: "password"}}
+    end
+    follow_redirect! # POSTリクエストを送信した結果を見て、指定されたリダイレクト先に移動するメソッド
+    assert_template 'users/show/'
+  end
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {user: {name: "",
+                                       email: "user@invalid",
+                                       password: "foo",
+                                       password_confirmation: "bar"}}
+    end
+    assert_template 'users/new'
+  end
+end
