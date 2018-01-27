da5e63dcc977da8880e1d8d8e214c689aa311dc9
muttsu-623 <mutsumi1996@gmail.com>
2018-01-27 20:50:48 +0900

    Add flash message(when login failed)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,18 @@ class SessionsController < ApplicationController
   def new
 
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase) # emailがない場合，nilが返る
+    if user && user.authenicate(params[:session][:password]) # userがいて，passwordが等しい場合true
+      redirect_to
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
 end
