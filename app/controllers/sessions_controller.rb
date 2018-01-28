3230691ec21ce309766f0c54cb0cbc3e14bcfe15
muttsu-623 <mutsumi1996@gmail.com>
2018-01-28 11:12:50 +0900

    Add cookies system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase) # emailがない場合，nilが返る
     if user && user.authenticate(params[:session][:password]) # userがいて，passwordが等しい場合true
       log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
