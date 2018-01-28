4affdb70a3b859eaa5de0e66ef13445796320021
muttsu-623 <mutsumi1996@gmail.com>
2018-01-28 18:29:29 +0900

    CorrectUser can access edit and update, and Add test of them

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,10 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token # 記憶トークンの永続cookies作成
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   # 現在ログイン中のユーザーを返る（いる場合）
   def current_user
     if (user_id = session[:user_id])
