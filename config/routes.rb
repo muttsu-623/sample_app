b9d406a6cdaccff18cd34c66923b930452195d8b
muttsu-623 <mutsumi1996@gmail.com>
2018-01-29 16:35:29 +0900

    Create account_activation mailer test

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :acount_activations, only:[:edit]
+  resources :account_activations, only:[:edit]
 end
